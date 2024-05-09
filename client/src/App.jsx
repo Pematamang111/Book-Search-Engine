a14c59dd54eb19a40cf9a9786f9e02612ccc63c9
Pema Sherpa <pematamang111@gmail.com>
2024-05-09 13:13:35 -0400

    add provider in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,24 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+//Importing an Apollo Provider to make every request work with the Apollo server.
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+	uri: '/graphql',
+	cache: new InMemoryCache(),
+});
 
 import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<Navbar />
+			<div className="flex-column justify-center align-center min-100-vh bg-primary">
+				<Outlet />
+			</div>
+		</ApolloProvider>
+	);
 }
 
 export default App;
